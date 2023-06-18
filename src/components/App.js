c7b6bb3313199e972709a5f53fced5782dd5be04
Pranav Pokale <pranavpokale50034@gmail.com>
2023-06-18 13:52:36 +0530

    SUBMIT

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,49 @@
-import React, {Component, useState} from "react";
+import React, { useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    render() {
+const App = ()=>{
+        
+        let [status, setStatus] = useState()
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
+        let relation = ["Friends", "Love", "Affection", "Marriage", "Enemy", "Siblings"]
+
+        function checkStatus(e) {
+            e.preventDefault();
+            if (
+                e.target[0].value.trim() === "" ||
+               e.target[1].value.trim() === ""
+               ) {
+                   setStatus("Please Enter valid input");
+                   return
+                }
+                
+                let s1 = e.target[0].value.trim().split('');
+                let s2 = e.target[1].value.trim().split('');
+                
+                setStatus(relation[(s1.length+s2.length)%6])
+
+        }
+
+        return (
+           <div id="main">
+              <form onSubmit={checkStatus}>
+                 <input placeholder="Enter first name" data-testid="input1" type="text" />
+                 <input placeholder="Enter second name" data-testid="input2" type="text" />
+                 <button data-testid="calculate_relationship" type="submit">
+                    Calculate Relationship Future
+                 </button>
+                 <button data-testid="clear" type="submit">
+                    Clear
+                 </button>
+              </form>
+              {
+                setStatus && (
+                    <h3 data-testid="answer">{status}</h3>
+                )
+              }
+           </div>
+        );
     }
-}
 
 
 export default App;
