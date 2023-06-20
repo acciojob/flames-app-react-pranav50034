23ab6c1d5dfa6622d70c247cd602a4ccc6f463ca
Pranav Pokale <pranavpokale50034@gmail.com>
2023-06-20 14:24:03 +0530

    great

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,16 @@
-import React, { useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+import Relation from "./Relation";
 
-const App = ()=>{
-        
-        let [status, setStatus] = useState()
-
-        let relation = ["Friends", "Love", "Affection", "Marriage", "Enemy", "Siblings"]
-
-        function checkStatus(e) {
-            e.preventDefault();
-            if (
-                e.target[0].value.trim() === "" ||
-               e.target[1].value.trim() === ""
-               ) {
-                   setStatus("Please Enter valid input");
-                   return
-                }
-                
-                let s1 = e.target[0].value.trim().split('');
-                let s2 = e.target[1].value.trim().split('');
-                
-                setStatus(relation[(s1.length+s2.length)%6])
-
-        }
-
-        return (
-           <div id="main">
-              <form onSubmit={checkStatus}>
-                 <input placeholder="Enter first name" data-testid="input1" type="text" />
-                 <input placeholder="Enter second name" data-testid="input2" type="text" />
-                 <button data-testid="calculate_relationship" type="submit">
-                    Calculate Relationship Future
-                 </button>
-                 <button data-testid="clear" type="submit">
-                    Clear
-                 </button>
-              </form>
-              {
-                setStatus && (
-                    <h3 data-testid="answer">{status}</h3>
-                )
-              }
-           </div>
-        );
-    }
-
+class App extends Component {
+   render() {
+      return (
+         <div id="main">
+            {/* Do not remove the main div */}
+            <Relation />
+         </div>
+      );
+   }
+}
 
 export default App;
